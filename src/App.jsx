f0fcec4fa12463641412fd062f2c9272d7a0dfd8
Duru2 <nickchanhee@gmail.com>
2025-04-21 12:55:16 +1000

    done for auth frontend with fetching backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SignIn from './sign-in/SignIn.jsx'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 
+import SignIn from './sign-in/SignIn.jsx';
+import SignUp from './sign-up/SignUp.jsx';
+// import Dashboard from './dashboard/Dashboard.jsx';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <SignIn />
-    </>
-  )
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/SignUp" element={<SignUp />} />
+            {/* <Route path="/checkout" element={<Checkout />} /> */}
+            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+            {/* <Route path="/MarketingPage" element={<MarketingPage />} /> */}
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

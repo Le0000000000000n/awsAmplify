46331a9de7d217ab81c14e3824d6edddae0e0d0e
Duru2 <nickchanhee@gmail.com>
2025-04-21 15:16:53 +1000

    replace backend link with proxy

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/proxy': {
-                target: 'https://e5lpxos917.execute-api.us-east-1.amazonaws.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/proxy/, '')
-            }
-        }
-    }
+    plugins: [react()]
 });
   
